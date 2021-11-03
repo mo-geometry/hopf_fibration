43affa82d12ee712757cbbce8e73cf32a2b684b2
mo-geometry <62537514+mo-geometry@users.noreply.github.com>
2021-11-03 21:49:49 +0000

    Delete matlab directory

--- matlab/src/angles4mPts.m
+++
@@ -1,5 +0,0 @@
-function angles = angles4mPts(pts)
-
-% pts = [cos(angles.theta),sin(angles.theta).*sin(angles.phi),sin(angles.theta).*cos(angles.phi)];
-angles.theta = acos(pts(:,1,:));
-angles.phi(:,1,:) = mod(atan2(pts(:,2,:),pts(:,3,:)),2*pi);
